8199daa43d8dff312161d737bb24d76165039e81
cainydev <wajo432@gmail.com>
2025-09-04 00:11:58 +0200

    Bootstrap days, restore input

--- bin/registry.ml
+++ bin/registry.ml
@@ -3,11 +3,11 @@ let days : (module Day.S) option array = [|
 	Some (module Aoc_days.Day02.Solution);
 	Some (module Aoc_days.Day03.Solution);
 	Some (module Aoc_days.Day04.Solution);
-	None;
+	Some (module Aoc_days.Day05.Solution);
 	Some (module Aoc_days.Day06.Solution);
 	Some (module Aoc_days.Day07.Solution);
-	None;
-	None;
+	Some (module Aoc_days.Day08.Solution);
+	Some (module Aoc_days.Day09.Solution);
 	Some (module Aoc_days.Day10.Solution);
 	Some (module Aoc_days.Day11.Solution);
 	None;
